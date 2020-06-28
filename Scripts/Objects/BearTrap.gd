ab8fe39b973c0aaf404e85f66b94f549af103122
Lynext <lynext12@gmail.com>
2020-06-28 19:19:05 +0300

    Lynext
    Trap animation

--- Scripts/Objects/BearTrap.gd
+++ Scripts/Objects/BearTrap.gd
@@ -8,11 +8,20 @@ var id
 var trappedPlayer = -1
 var trapTimeRemaining = 0
 
+var animationPlayed = false setget setAnimationPlayed
+
+func setAnimationPlayed (v):
+	animationPlayed = v
+	if animationPlayed:
+		$AnimatedSprite.play("default")
+
 func _ready():
 	if Vars.myTeam != Vars.objects[whoSummoned]["team"]:
 		modulate = Vars.teams[Vars.objects[whoSummoned]["team"]]["color"].blend(Color(1,1,1,0.3))
 func _process(delta):
 	if trappedPlayer == Client.selfPeerID:
+		if Vars.objects[Client.selfPeerID].get_node("Sprite").animation != "rooted":
+			Vars.objects[Client.selfPeerID].get_node("Sprite").play("rooted")
 		Vars.objects[Client.selfPeerID].canMove = false
 		Vars.objects[Client.selfPeerID].animationPlaying = false
 	if Vars.roomMaster == Client.selfPeerID:
@@ -38,6 +47,8 @@ func _on_Area2D_body_entered(body):
 			return
 		if body.is_in_group("Player") && Vars.objects[body.id]["team"] != Vars.objects[whoSummoned]["team"]:
 			trapTimeRemaining = 2
+			get_tree().root.get_node("Main").rpc_id(1,"objectUpdated",Client.selfPeerID,id,{"animationPlayed": true})
+			$AnimatedSprite.play("default")
 			trappedPlayer = body.id
 
 func _on_BearTrap_tree_exited():
