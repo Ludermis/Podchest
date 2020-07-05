14ea9f415c7daa177d2428c51238b328bb98e52b
Lynext <lynext12@gmail.com>
2020-07-05 20:41:43 +0300

    Lynext
    Xedarin class & buying character

--- Scripts/Scenes/StoreScene.gd
+++ Scripts/Scenes/StoreScene.gd
@@ -12,6 +12,8 @@ remote func accountInfoRefreshed (info):
 func refreshStore ():
 	for child in $"TabContainer/Skins/Panel/ScrollContainer/VBoxContainer/HBoxContainer".get_children():
 		child.queue_free()
+	for child in $"TabContainer/Characters/Panel/ScrollContainer/VBoxContainer/HBoxContainer".get_children():
+		child.queue_free()
 	$MoneyPanel/CoinLabel.text = str(Vars.accountInfo["gold"])
 	$MoneyPanel/APLabel.text = str(Vars.accountInfo["AP"])
 	for i in Vars.store["skins"]:
@@ -32,6 +34,21 @@ func refreshStore ():
 				node.get_node("CoinSprite").play("AP")
 				node.get_node("CoinSprite").scale = Vector2(0.4,0.4)
 			$"TabContainer/Skins/Panel/ScrollContainer/VBoxContainer/HBoxContainer".add_child(node)
+		
+		for i in Vars.store["characters"]:
+			if Vars.accountInfo["ownedCharacters"].has(i):
+				continue
+			var node = preload("res://Prefabs/UI/ItemCase.tscn").instance()
+			node.itemType = "character"
+			node.character = i
+			node.item = i
+			node.get_node("Label").text = i
+			node.get_node("TextureRect").texture = load("res://Sprites/UI/Characters/" + i + ".png")
+			node.get_node("MoneyLabel").text = str(Vars.store["characters"][i]["priceGold"])
+			if Vars.accountInfo["gold"] < Vars.store["characters"][i]["priceGold"]:
+				node.disabled = true
+				node.self_modulate = Color.darkred
+			$"TabContainer/Characters/Panel/ScrollContainer/VBoxContainer/HBoxContainer".add_child(node)
 
 remote func updateStore (store):
 	Vars.store = store
