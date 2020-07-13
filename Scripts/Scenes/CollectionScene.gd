6453178f603869a24fd5df4d86e3681bd36d301d
Lynext <lynext12@gmail.com>
2020-07-13 17:43:50 +0300

    Lynext
    Build 9

--- Scripts/Scenes/CollectionScene.gd
+++ Scripts/Scenes/CollectionScene.gd
@@ -2,17 +2,40 @@ extends Node2D
 
 
 func _ready():
+	rpc_id(1,"demandStore",Client.selfPeerID)
+
+func characters ():
+	$AllLabel.text = "All Characters\n" + str(Vars.store["characters"].keys().size() + 1)
+	$YourLabel.text = "Your Characters\n" + str(Vars.accountInfo["ownedCharacters"].size())
+	for child in $"Panel/ScrollContainer/VBoxContainer/HBoxContainer".get_children():
+		child.queue_free()
 	for i in Vars.accountInfo["ownedCharacters"]:
 		var node = preload("res://Prefabs/UI/CharacterRect.tscn").instance()
 		node.name = i
 		node.texture = load("res://Sprites/UI/Characters/" + i + ".png")
 		node.get_node("Label").text = i
-		$"TabContainer/Characters/ItemList".add_child(node)
+		$"Panel/ScrollContainer/VBoxContainer/HBoxContainer".add_child(node)
+
+func skins ():
+	var totalSkinCount = 0
+	var mySkinCount = 0
+	for i in Vars.store["skins"]:
+		totalSkinCount += Vars.store["skins"][i].keys().size()
+	for i in Vars.accountInfo["ownedSkins"]:
+		mySkinCount += Vars.accountInfo["ownedSkins"][i].size()
+	$AllLabel.text = "All Skins\n" + str(totalSkinCount)
+	$YourLabel.text = "Your Skins\n" + str(mySkinCount)
+	for child in $"Panel/ScrollContainer/VBoxContainer/HBoxContainer".get_children():
+		child.queue_free()
 	for i in Vars.accountInfo["ownedCharacters"]:
-		if	Vars.accountInfo["ownedSkins"].has(i):
+		if Vars.accountInfo["ownedSkins"].has(i):
 			for j in Vars.accountInfo["ownedSkins"][i]:
 				var node = preload("res://Prefabs/UI/CharacterRect.tscn").instance()
 				node.name = j
 				node.texture = load("res://Sprites/UI/Characters/" + j + ".png")
 				node.get_node("Label").text = j
-				$"TabContainer/Skins/ItemList".add_child(node)
+				$"Panel/ScrollContainer/VBoxContainer/HBoxContainer".add_child(node)
+
+remote func updateStore (store):
+	Vars.store = store
+	characters()
