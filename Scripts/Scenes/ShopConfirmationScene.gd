1745289e054408d5469fd38cb2cd7cbae5012b45
Lynext <lynext12@gmail.com>
2020-07-18 20:17:01 +0300

    Lynext
    New Gold & AP labels

--- Scripts/Scenes/ShopConfirmationScene.gd
+++ Scripts/Scenes/ShopConfirmationScene.gd
@@ -14,13 +14,21 @@ func _ready():
 	$Panel/TextureRect.texture = texture
 	$Panel/APButton/HBoxContainer/Label.text = str(AP)
 	$Panel/GoldButton/HBoxContainer/Label.text = str(gold)
+	$Panel/NewGold.text = "New Gold : " + str(Vars.accountInfo["gold"] - gold)
+	$Panel/NewAP.text = "New AP : " + str(Vars.accountInfo["AP"] - AP)
 	if Vars.accountInfo["gold"] < gold:
 		$Panel/GoldButton.disabled = true
+		$Panel/NewGold.modulate = Color.red
+		$Panel/NewGold.text = "Not enough Gold"
 	if Vars.accountInfo["AP"] < AP:
 		$Panel/APButton.disabled = true
+		$Panel/NewAP.modulate = Color.red
+		$Panel/NewAP.text = "Not enough AP"
 	if !APEnabled:
+		$Panel/NewAP.visible = false
 		$Panel/APButton.visible = false
 	if !goldEnabled:
+		$Panel/NewGold.visible = false
 		$Panel/GoldButton.visible = false
 
 func _on_CloseButton_pressed():
