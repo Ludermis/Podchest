65da876a42760b4931226f08957eb66e01bc6de6
Lynext <lynext12@gmail.com>
2020-07-22 02:26:04 +0300

    Lynext
    Build 19

--- Scripts/Scenes/AdminPanelScene.gd
+++ Scripts/Scenes/AdminPanelScene.gd
@@ -2,18 +2,21 @@ extends Node2D
 
 
 func _ready():
-	rpc_id(1,"demandAdminInfo",Client.selfPeerID)
+	rpc_id(1,"demandAdminInfo",Client.selfPeerID,{"type": "main"})
 
 func _on_CancelButton_pressed():
 	get_tree().change_scene("res://Prefabs/Scenes/MainScene.tscn")
 
-remote func gotAdminInfo (dict):
-	Vars.adminInfo = dict
-	for i in Vars.adminInfo["logs"]:
-		$LogsList.add_item(i)
-	$LogsList.select(0)
-	_on_LogsList_item_selected(0)
+remote func gotAdminInfo (demand, dict):
+	if demand["type"] == "main":
+		Vars.adminInfo = dict
+		for i in Vars.adminInfo["logs"]:
+			$LogsList.add_item(i)
+		$LogsList.select(0)
+		_on_LogsList_item_selected(0)
+	elif demand["type"] == "getLog":
+		$RichTextLabel.bbcode_text = Vars.logBBCode(dict["log"])
 
 
 func _on_LogsList_item_selected(index):
-	$RichTextLabel.bbcode_text = Vars.logBBCode(Vars.adminInfo["logs"][$LogsList.get_item_text(index)])
+	rpc_id(1,"demandAdminInfo",Client.selfPeerID,{"type": "getLog", "which": $LogsList.get_item_text(index)})
